4953281ecc535f6fec9b695279e9235065f8d405
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-11-10 11:51:22 +0530

    [shweta] added the UC1 code for tic tac toe game

---
+++ TicTacToeGame/TicTacGameClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+   public class TicTacGameClass
+    {
+
+        public void createBoard()
+        {
+            char[] board = new char[10];
+            board[1]=' ';
+            board[2] = ' ';
+            board[3] = ' ';
+            board[4] = ' ';
+            board[5] = ' ';
+            board[6] = ' ';
+            board[7] = ' ';
+            board[8] = ' ';
+            board[9] = ' ';
+
+        }
+    }
+}
